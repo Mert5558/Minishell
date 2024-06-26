5fba7a3c7a9182f340770482172a1fac8d01b90f
Mert5558 <149884808+Mert5558@users.noreply.github.com>
2024-06-26 15:37:07 +0200

    Add files via upload

--- minishell.c
+++ minishell.c
@@ -6,11 +6,17 @@
 /*   By: merdal <merdal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 16:19:36 by merdal            #+#    #+#             */
-/*   Updated: 2024/06/18 16:01:19 by merdal           ###   ########.fr       */
+/*   Updated: 2024/06/26 15:33:43 by merdal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char **argv)
+#include "minishell.h"
+
+int main(int argc, char **argv, char **envp)
 {
-	
+	char *input;
+
+	input = readline("minishell$ ");
+	printf("%s\n", input);
+	return (0);
 }
