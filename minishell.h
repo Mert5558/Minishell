08a58cab69d5ac05d4d6f739d176bd1bdb2539a3
Mert5558 <149884808+Mert5558@users.noreply.github.com>
2024-06-27 16:05:36 +0200

    Add files via upload

---
+++ minishell.h
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: merdal <merdal@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/24 13:07:42 by merdal            #+#    #+#             */
+/*   Updated: 2024/06/27 11:46:46 by merdal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+typedef enum e_token_type
+{
+	TOKEN_CMD,
+	TOKEN_ARG,
+	TOKEN_PIPE,
+	TOKEN_OPERATOR,
+	TOKEN_REDIR_IN,
+	TOKEN_REDIR_OUT,
+	TOKEN_REDIR_APPEND,
+}t_token_type;
+
+
+typedef struct s_cmd
+{
+	char	*cmd;
+	char	**args;
+	int		input_fd;
+	int		output_fd;
+}t_cmd;
+
+typedef struct s_token
+{
+	char			*value;
+	t_token_type	type;
+}t_token;
+
+typedef struct s_env
+{
+	char	*key;
+	char	*value;
+}t_env;
+
+
+
+
+
+
+#endif
