1d596386dd13e684eb01c7295c76f76e2c63129f
Mert5558 <149884808+Mert5558@users.noreply.github.com>
2024-07-22 12:04:09 +0200

    minor changes and cleaned ft_tokenize.c

--- ft_tokenize.c
+++ ft_tokenize.c
@@ -6,78 +6,83 @@
 /*   By: merdal <merdal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 15:35:03 by merdal            #+#    #+#             */
-/*   Updated: 2024/07/19 16:04:37 by merdal           ###   ########.fr       */
+/*   Updated: 2024/07/22 11:52:57 by merdal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-#include "minishell.h"
-
-int ft_count_tokens(char *input)
+int	ft_count_tokens(char *input)
 {
-    int count = 0;
-    int flag = 0;
-    int quotes = 0;
+	int	count;
+	int	flag;
+	int	quotes;
 
-    while (*input)
-    {
-        if (*input == '|' || *input == '&' || *input == ';' || *input == '<')
-            count += !quotes;
-        else if (*input == '>')
-        {
-            if (*(input + 1) == '>')
-                input++;
-            count += !quotes;
-        }
-        if ((*input == '"' || *input == '\'') && *input == *(input + 1))
-            input++;
-        else if (*input == '"' || *input == '\'')
-            quotes = !quotes;
-        if (*input != ' ' && *input != '|' && *input != '&' && *input != ';' && *input != '>' && *input != '<' && !flag && !quotes)
-        {
-            flag = 1;
-            count++;
-        }
-        else if (*input == ' ' || *input == '|' || *input == '&' || *input == ';' || *input == '>' || *input == '<')
-            flag = 0;
-        input++;
-    }
-    return (count);
+	count = 0;
+	flag = 0;
+	quotes = 0;
+	while (*input)
+	{
+		if (*input == '|' || *input == '&' || *input == ';' || *input == '<')
+			count += !quotes;
+		else if (*input == '>')
+		{
+			if (*(input + 1) == '>')
+				input++;
+			count += !quotes;
+		}
+		if ((*input == '"' || *input == '\'') && *input == *(input + 1))
+			input++;
+		else if (*input == '"' || *input == '\'')
+			quotes = !quotes;
+		if (*input != ' ' && *input != '|' && *input != '&' && *input != ';'
+			&& *input != '>' && *input != '<' && !flag && !quotes)
+		{
+			flag = 1;
+			count++;
+		}
+		else if (*input == ' ' || *input == '|' || *input == '&'
+			|| *input == ';' || *input == '>' || *input == '<')
+			flag = 0;
+		input++;
+	}
+	return (count);
 }
 
 int	ft_token_len(char *input, int i)
 {
-    size_t	len;
-    char	quotes;
+	size_t	len;
+	char	quotes;
 
-    len = 0;
-    if (input[i] == '|' || input[i] == '&' || input[i] == ';' || input[i] == '>' || input[i] == '<')
-    {
-        if (input[i] == '>' && input[i + 1] == '>')
-            return (2);
-        return (1);
-    }
-    while (input[i] && input[i] != ' ' && input[i] != '|' && input[i] != '&' && input[i] != ';' && input[i] != '>' && input[i] != '<')
-    {
-        if (input[i] == '\"' || input[i] == '\'')
-        {
-            quotes = input[i++];
-            while (input[i] && input[i] != quotes)
-                i++;
-            if (input[i] == quotes)
-                i++;
-        }
-        else
-            i++;
-        len++;
-    }
-    return (len);
+	len = 0;
+	if (input[i] == '|' || input[i] == '&' || input[i] == ';'
+		|| input[i] == '>' || input[i] == '<')
+	{
+		if (input[i] == '>' && input[i + 1] == '>')
+			return (2);
+		return (1);
+	}
+	while (input[i] && input[i] != ' ' && input[i] != '|' && input[i] != '&'
+		&& input[i] != ';' && input[i] != '>' && input[i] != '<')
+	{
+		if (input[i] == '\"' || input[i] == '\'')
+		{
+			quotes = input[i++];
+			while (input[i] && input[i] != quotes)
+				i++;
+			if (input[i] == quotes)
+				i++;
+		}
+		else
+			i++;
+		len++;
+	}
+	return (len);
 }
 
 char	*ft_cut(char *input, int i, int len)
 {
-	char 	*cut;
+	char	*cut;
 	int		j;
 
 	j = 0;
@@ -96,10 +101,11 @@ char	*ft_cut(char *input, int i, int len)
 
 char	**ft_create_array(char *input, t_env *env)
 {
-	int	i;
-	int j;
-	int	x;
-	int	len;
+	int		i;
+	int		j;
+	int		x;
+	int		len;
+	char	quote;
 	char	**array;
 
 	i = 0;
@@ -119,6 +125,29 @@ char	**ft_create_array(char *input, t_env *env)
 			len = ft_token_len(input, i);
 			array[j] = ft_handle_dollar(ft_cut(input, i, len), env);
 			i = i + len;
+			j++;
+		}
+		else if (input[i] == '"' || input[i] == '\'')
+		{
+			quote = input[i++];
+			len = 0;
+			while (input[i + len] && input[i + len] != quote)
+				len++;
+			len += 2;
+			array[j] = malloc(sizeof(char) * (len + 1));
+			if (!array[j])
+				return (NULL);
+			array[j][0] = quote;
+			x = 1;
+			while (x < len - 1)
+			{
+				array[j][x] = input[i];
+				x++;
+				i++;
+			}
+			array[j][x] = quote;
+			array[j][x + 1] = '\0';
+			i++;
 			j++;
 		}
 		else
