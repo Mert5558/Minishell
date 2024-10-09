500a327b3c4060d1cd66888cba145f3d33d4a87d
Mert5558 <149884808+Mert5558@users.noreply.github.com>
2024-10-09 16:24:58 +0200

    fixed pipe with redirs

--- set_fds.c
+++ set_fds.c
@@ -6,26 +6,30 @@
 /*   By: merdal <merdal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 14:51:08 by merdal            #+#    #+#             */
-/*   Updated: 2024/10/02 13:48:17 by merdal           ###   ########.fr       */
+/*   Updated: 2024/10/09 16:01:31 by merdal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// void	ft_fix_struct(t_cmd *cmd_head)
-// {
-// 	t_cmd	*current;
+t_cmd	*ft_fix_struct(t_cmd *cmd_head)
+{
+	t_cmd	*current;
+	t_cmd	*delete;
 
-// 	current = cmd_head;
-// 	while (current)
-// 	{
-// 		if (current->operator && ft_strcmp(current->operator, "<<") == 0)
-// 		{
-			
-// 		}
-// 		current = current->next;
-// 	}
-// }
+	current = cmd_head;
+	if (current->operator && ft_strcmp(current->operator, "<") == 0
+		&& current->next->operator && ft_strcmp(current->next->operator, "|") == 0)
+	{
+		current->output_fd = current->next->output_fd;
+
+		delete = current->next;
+		current->next = current->next->next;
+		
+		//free the deleted node
+	}
+	return (cmd_head);
+}
 
 t_cmd	*ft_set_fds(t_cmd *temp)
 {
@@ -46,94 +50,6 @@ t_cmd	*ft_set_fds(t_cmd *temp)
 			ft_pipe(temp);
 		temp = temp->next;
 	}
+	new_head = ft_fix_struct(new_head);
 	return (new_head);
 }
-
-
-// t_cmd	*ft_set_fds(t_cmd *temp)
-// {
-// 	t_cmd	*new_head = temp;
-	
-// 	while (temp->next)
-// 	{
-// 		if (ft_strcmp(temp->operator, ">") == 0)
-// 		{
-// 			temp->output_fd = open(temp->next->args[0], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-// 			if (temp->output_fd == -1)
-// 			{
-// 				perror("open");
-// 				return (NULL);
-// 			}
-// 		}
-// 		else if(ft_strcmp(temp->operator, ">>") == 0)
-// 		{
-// 			temp->output_fd = open(temp->next->args[0], O_WRONLY | O_CREAT | O_APPEND, 0644);
-// 			if (temp->output_fd == -1)
-// 			{
-// 				perror("open");
-// 				return (NULL);
-// 			}
-// 		}
-// 		else if(ft_strcmp(temp->operator, "<") == 0)
-// 		{
-// 			temp->input_fd = open(temp->next->args[0], O_RDONLY);
-// 			if (temp->input_fd == -1)
-// 			{
-// 				perror("open");
-// 				return (NULL);
-// 			}
-// 		}
-// 		else if(ft_strcmp(temp->operator, "<<") == 0)
-// 		{
-// 			int		pipe_fd[2];
-// 			char	*input;
-
-// 			if (pipe(pipe_fd) == -1)
-// 			{
-// 				perror("pipe");
-// 				return (NULL);
-// 			}
-// 			while (1)
-// 			{
-// 				input = readline("heredoc> ");
-// 				if (ft_strcmp(input, temp->next->args[0]) == 0)
-// 				{
-// 					free(input);
-// 					break;
-// 				}	
-// 				write(pipe_fd[1], input, strlen(input));
-// 				write(pipe_fd[1], "\n", 1);
-// 				free(input);
-// 			}
-// 			close(pipe_fd[1]);
-
-// 			temp->input_fd = pipe_fd[0];
-// 		}
-// 		else if(ft_strcmp(temp->operator, "|") == 0)
-// 		{
-// 			int	pipe_fd[2];
-// 			if (pipe(pipe_fd) == -1)
-// 			{
-// 				perror("pipe");
-// 				return (NULL);
-// 			}
-// 			temp->output_fd = pipe_fd[0];
-// 			if(temp->output_fd == -1)
-// 			{
-// 				perror("pipe");
-// 				return (NULL);
-// 			}
-// 			if (temp->next != NULL)
-// 			{
-// 				temp->next->input_fd = pipe_fd[1];
-// 				if (temp->next->input_fd == -1)
-// 				{
-// 					perror("pipe");
-// 					return (NULL);
-// 				}
-// 			}
-// 		}
-// 		temp = temp->next;
-// 	}
-// 	return (new_head);
-// }
